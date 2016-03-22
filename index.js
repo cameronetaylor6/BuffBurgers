6e5f98549e70667bd2ddba17c815c913c4114397
Alex-Luo <allu6345@colorado.edu>
2016-03-21 22:13:45 -0500

    Got MongoDB server working on local host

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
 var express = require('express');	
 var app = express();
 var fs = require('fs');
+var MongoClient = require('mongodb').MongoClient;
+var assert = require('assert');
+
+var url = 'mongodb://localhost:27017/test';
+MongoClient.connect(url, function(err, db) {
+				  assert.equal(null, err);
+					  console.log("Connected correctly to server.");
+						  db.close();
+});
 
 app.get('/', function (req, res){
 	fs.readFile('html/index.html', function(err, text){
