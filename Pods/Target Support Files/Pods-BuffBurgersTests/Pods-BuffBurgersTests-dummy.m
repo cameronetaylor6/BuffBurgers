e3ad8ce4682bcf7626879cc9ac18fdf323933639
alexwalling <alex.walling19@gmail.com>
2016-04-18 16:23:34 -0600

    add cocoapods stuff

---
+++ Pods/Target Support Files/Pods-BuffBurgersTests/Pods-BuffBurgersTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_BuffBurgersTests : NSObject
+@end
+@implementation PodsDummy_Pods_BuffBurgersTests
+@end
---
+++ Pods/Target Support Files/Pods-BuffBurgersUITests/Pods-BuffBurgersUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_BuffBurgersUITests : NSObject
+@end
+@implementation PodsDummy_Pods_BuffBurgersUITests
+@end
