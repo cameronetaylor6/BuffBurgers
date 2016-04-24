d941ba9e66268ded168ed02677fdf8e9ef4c8998
Callie Jones <calliejones@Callies-Mac.local>
2016-04-24 13:20:53 -0700

    storyboard segues and password reset/change functionality

---
+++ BuffBurgers/AccountManageVC.swift
@@ -0,0 +1,68 @@
+//
+//  AccountManageVC.swift
+//  BuffBurgers
+//
+//  Created by Callie Jones on 4/21/16.
+//  Copyright © 2016 CSCI 3308. All rights reserved.
+//
+
+import UIKit
+import Firebase
+
+class AccountManageVC: UIViewController {
+    
+    @IBOutlet var tempPassword : UITextField!
+    @IBOutlet var newPassword : UITextField!
+    @IBOutlet var confirmPassword : UITextField!
+    let ref = Firebase(url: "https://buffburgers.firebaseio.com")
+    
+    @IBAction func passwordchangedTapped(sender : UIButton){
+        print(userID,newPassword,confirmPassword)
+        
+        if newPassword.text! == confirmPassword.text!
+        {
+            print(userID)
+            ref.changePasswordForUser(userID, fromOld: tempPassword.text!, toNew: confirmPassword.text!, withCompletionBlock: {
+                error in
+                if error != nil
+                {
+                    // There was an error processing the request
+                    let alertController = UIAlertController(title: "Passwor Reset Failed!", message: "This Account Does Not Exist", preferredStyle: .Alert)
+                    let OKAction = UIAlertAction(title: "OK", style: .Cancel) { action -> Void in }
+                    alertController.addAction(OKAction)
+                    self.presentViewController(alertController, animated: true, completion: nil)
+                    print("ther was an error")
+
+                }
+                else
+                {
+                    // Password changed successfully
+                    let alertController = UIAlertController(title: "Congrats!", message: "Your Password has been reset to \(self.newPassword.text!)", preferredStyle: .Alert)
+                    let OKAction = UIAlertAction(title: "OK", style: .Cancel) { action -> Void in }
+                    alertController.addAction(OKAction)
+                    self.presentViewController(alertController, animated: true, completion: nil)
+                    print("NO error")
+                    self.performSegueWithIdentifier("recent-segue", sender: nil)
+                    
+                }
+            })
+        }
+        else
+        {
+            let alertController = UIAlertController(title: "Password Reset Failed!", message: "Reconfirm Your Password", preferredStyle: .Alert)
+            let OKAction = UIAlertAction(title: "OK", style: .Cancel) { action -> Void in }
+            alertController.addAction(OKAction)
+            self.presentViewController(alertController, animated: true, completion: nil)
+            print("Error")
+        }
+
+    }
+    
+    @IBAction func signoutTapped(sender : UIButton){
+        
+        ref.unauth()
+        self.performSegueWithIdentifier("login-segue", sender: nil)
+        
+    }
+    
+}
--- BuffBurgers/SignupVC.swift
+++ BuffBurgers/SignupVC.swift
@@ -11,6 +11,7 @@ class SignupVC: UIViewController, UITextFieldDelegate {
     @IBOutlet var txtUsername : UITextField!
     @IBOutlet var txtPassword : UITextField!
     @IBOutlet var txtConfirmPassword : UITextField!
+    var userMade = false
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -33,63 +34,82 @@ class SignupVC: UIViewController, UITextFieldDelegate {
         let userRef = Firebase(url: "https://buffburgers.firebaseio.com")
         
         let email = txtUsername.text!
-        let password = txtPassword.text!
-        let confirm_password = txtConfirmPassword.text!
+        //let password = txtPassword.text!
+        //let confirm_password = txtConfirmPassword.text!
         
-        if email != "" && password != ""
+        if email != ""
         {
-            if password == confirm_password
+            if email.lowercaseString.rangeOfString("@colorado.edu") != nil
             {
-                if email.lowercaseString.rangeOfString("@colorado.edu") != nil
-                {
-                    //create user with fake password
-                    DataService.dataservice.refFirebase.createUser(email, password: "password", withValueCompletionBlock: { error, authData in
+                //create user with fake password
+                DataService.dataservice.refFirebase.createUser(email, password: "password", withValueCompletionBlock: { error, authData in
+                    
+                    if error != nil{
+                        print("there was an eror")
+                        print(error)
                         
-                        if error != nil{
-                            print("there was an eror")
-                            print(error)
-
-                            let alertController = UIAlertController(title: "Sign Up Failed!", message: "Account Already Exists", preferredStyle: .Alert)
-                            let OKAction = UIAlertAction(title: "OK", style: .Default) { (action) -> Void in }
+                        let alertController = UIAlertController(title: "Sign Up Failed!", message: "Account Already Exists", preferredStyle: .Alert)
+                        let OKAction = UIAlertAction(title: "OK", style: .Default) { (action) -> Void in }
+                        alertController.addAction(OKAction)
+                        self.presentViewController(alertController, animated: true, completion: nil)
+                        
+                        
+                    }
+                        
+                    else {
+                        NSUserDefaults.standardUserDefaults().setValue(authData["uid"], forKey: "uid")
+                        DataService.dataservice.refFirebase.authUser(email, password: "password", withCompletionBlock: { error, result in
+                            
+                            let user = ["email": email]
+                            DataService.dataservice.createUser(result.uid, user: user)//
+                            userID = email
+                            self.userMade = true
+                            //self.performSegueWithIdentifier("signed_up", sender: nil)
+                            
+                        })
+                        print("success")
+                    }
+                })
+                //send password reset
+                
+                if userMade == true
+                {
+                    DataService.dataservice._refFirebase.resetPasswordForUser(email, withCompletionBlock: { error in
+                        
+                        if error != nil
+                        {
+                            // There was an error processing the request
+                            let alertController = UIAlertController(title: "Password Reset Failed!", message: "There is no Account for this Email", preferredStyle: .Alert)
+                            let OKAction = UIAlertAction(title: "OK", style: .Cancel) { action -> Void in }
                             alertController.addAction(OKAction)
                             self.presentViewController(alertController, animated: true, completion: nil)
-                            
-                            
+                            print("NO error")
                         }
-                            
-                        else {
-                            NSUserDefaults.standardUserDefaults().setValue(authData["uid"], forKey: "uid")
-                            DataService.dataservice.refFirebase.authUser(email, password: password, withCompletionBlock: { error, result in
-                                
-                                let user = ["email": email]
-                                DataService.dataservice.createUser(result.uid, user: user)//
-                                self.performSegueWithIdentifier("signed_up", sender: nil)
-                                
-                            })
-                            print("success")
+                        else
+                        {
+                            // Password reset sent successfully
+                            let alertController = UIAlertController(title: "Password is Reset!", message: "Please Check Your Email", preferredStyle: .Alert)
+                            let OKAction = UIAlertAction(title: "OK", style: .Cancel) { action -> Void in }
+                            alertController.addAction(OKAction)
+                            self.presentViewController(alertController, animated: true, completion: nil)
                         }
-                        
-                        
                     })
                 }
                 else{
-                    let alertController = UIAlertController(title: "Sign in Failed!", message: "Please Use a Colorado Email", preferredStyle: .Alert)
-                    let OKAction = UIAlertAction(title: "OK", style: .Default) { (action) -> Void in }
-                    alertController.addAction(OKAction)
-                    self.presentViewController(alertController, animated: true, completion: nil)
+                    print("there was an error creating the account")
                 }
+                
             }
             else{
-                let alertController = UIAlertController(title: "Sign in Failed!", message: "Please Re-Enter Your Password", preferredStyle: .Alert)
+                let alertController = UIAlertController(title: "Sign in Failed!", message: "Please Use a Colorado Email", preferredStyle: .Alert)
                 let OKAction = UIAlertAction(title: "OK", style: .Default) { (action) -> Void in }
                 alertController.addAction(OKAction)
                 self.presentViewController(alertController, animated: true, completion: nil)
             }
-            
         }
             
         else{
-            let alertController = UIAlertController(title: "Sign in Failed!", message: "Please enter Email and Password", preferredStyle: .Alert)
+            let alertController = UIAlertController(title: "Sign in Failed!", message: "Please enter a Colorado Email", preferredStyle: .Alert)
             let OKAction = UIAlertAction(title: "OK", style: .Default) { (action) -> Void in }
             alertController.addAction(OKAction)
             self.presentViewController(alertController, animated: true, completion: nil)
@@ -104,6 +124,77 @@ class SignupVC: UIViewController, UITextFieldDelegate {
 
 
 }
+
+/*
+ @IBAction func signupTapped(sender : UIButton) {
+ let userRef = Firebase(url: "https://buffburgers.firebaseio.com")
+ 
+ let email = txtUsername.text!
+ let password = txtPassword.text!
+ let confirm_password = txtConfirmPassword.text!
+ 
+ if email != "" && password != ""
+ {
+ if password == confirm_password
+ {
+ if email.lowercaseString.rangeOfString("@colorado.edu") != nil
+ {
+ //create user with fake password
+ DataService.dataservice.refFirebase.createUser(email, password: "password", withValueCompletionBlock: { error, authData in
+ 
+ if error != nil{
+ print("there was an eror")
+ print(error)
+ 
+ let alertController = UIAlertController(title: "Sign Up Failed!", message: "Account Already Exists", preferredStyle: .Alert)
+ let OKAction = UIAlertAction(title: "OK", style: .Default) { (action) -> Void in }
+ alertController.addAction(OKAction)
+ self.presentViewController(alertController, animated: true, completion: nil)
+ 
+ 
+ }
+ 
+ else {
+ NSUserDefaults.standardUserDefaults().setValue(authData["uid"], forKey: "uid")
+ DataService.dataservice.refFirebase.authUser(email, password: password, withCompletionBlock: { error, result in
+ 
+ let user = ["email": email]
+ DataService.dataservice.createUser(result.uid, user: user)//
+ self.performSegueWithIdentifier("signed_up", sender: nil)
+ 
+ })
+ print("success")
+ }
+ 
+ 
+ })
+ }
+ else{
+ let alertController = UIAlertController(title: "Sign in Failed!", message: "Please Use a Colorado Email", preferredStyle: .Alert)
+ let OKAction = UIAlertAction(title: "OK", style: .Default) { (action) -> Void in }
+ alertController.addAction(OKAction)
+ self.presentViewController(alertController, animated: true, completion: nil)
+ }
+ }
+ else{
+ let alertController = UIAlertController(title: "Sign in Failed!", message: "Please Re-Enter Your Password", preferredStyle: .Alert)
+ let OKAction = UIAlertAction(title: "OK", style: .Default) { (action) -> Void in }
+ alertController.addAction(OKAction)
+ self.presentViewController(alertController, animated: true, completion: nil)
+ }
+ 
+ }
+ 
+ else{
+ let alertController = UIAlertController(title: "Sign in Failed!", message: "Please enter Email and Password", preferredStyle: .Alert)
+ let OKAction = UIAlertAction(title: "OK", style: .Default) { (action) -> Void in }
+ alertController.addAction(OKAction)
+ self.presentViewController(alertController, animated: true, completion: nil)
+ }
+ 
+ }
+
+ */
 
 
 
